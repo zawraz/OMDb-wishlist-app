9722017b159ef96da49c0277871113fc7e12e80e
Konrad Sawras <zawraz@gmail.com>
2022-06-02 22:11:59 +0200

    upgraded OMDb API data fetch

--- index.js
+++ index.js
@@ -1,38 +1,78 @@
-const goToWatchlistBtn = document.getElementById("go-to-watchlist-btn");
+const goToWishlistBtn = document.getElementById("go-to-wishlist-btn");
 const searchInput = document.querySelector('[name="movie-search"]');
 const searchBtn = document.getElementById("search-btn");
+const searchForm = document.getElementById("search-form");
 
-let getResults = async () => {
+let idArr;
+let movieArr;
+
+const getResults = async (e) => {
+  e.preventDefault();
+
   const resp = await fetch(
-    `https://www.omdbapi.com/?apikey=652b6583&s=gladiator`,
+    `https://www.omdbapi.com/?apikey=652b6583&s=${searchInput.value}`,
     { method: "GET" }
   );
   const data = await resp.json();
-  console.log(data);
+  searchForm.reset();
+
+  if (data.Response === "True") {
+    idArr = Array.from(Array(data.Search.length).keys()).map(
+      (movie) => data.Search[movie].imdbID
+    );
+
+    async function getData() {
+      const data = Promise.all(
+        idArr.map(
+          async (i) =>
+            await (
+              await fetch(`https://www.omdbapi.com/?apikey=652b6583&i=${i}`)
+            ).json()
+        )
+      );
+      return data;
+    }
+
+    getData().then((data) => {
+      console.log(data);
+    });
+
+    // movieArr = await Promise.all(
+    //   idArr.map(async (movieId) => {
+    //     await fetch(`https://www.omdbapi.com/?apikey=652b6583&i=${[movieId]}`, {
+    //       method: "GET",
+    //     });
+    //     console.log(movieArr);
+    //   })
+    // );
+    // console.log(movieArr);
+  } else {
+    console.log(data.Error);
+  }
 };
 
 searchBtn.addEventListener("click", getResults);
 
-// https://www.omdbapi.com/?apikey=652b6583&s=[title_here]
+// TODO: results in the OMDb: n (up top)
 
-{
-  /* <article class="width-wrapper flexbox">
-        <div class="movie-items flexbox">
-          <div class="movie-images">Movie image</div>
-          <div class="movie-side flexbox">
-            <div class="movie-title-row flexbox">
-              <h2 class="movie-title">Movie title</h2>
-              <span class="movie-rating">Movie rating</span>
-            </div>
-            <div class="movie-data-row flexbox">
-              <span class="movie-duration">51 min</span>
-              <span class="movie-genres">Drama, Sci-fi</span>
-              <button class="add-btn" type="button">Add to watchlist</button>
-            </div>
-            <div class="movie-desc-row">
-              <p class="movie-desc">Movie description</p>
-            </div>
-          </div>
-        </div>
-      </article> */
-}
+/* <article class="width-wrapper flexbox">
+  <div class="movie-items flexbox">
+    <div class="movie-images">Movie image</div>
+    <div class="movie-side flexbox">
+      <div class="movie-title-row flexbox">
+        <h2 class="movie-title">Movie title</h2>
+        <span class="movie-rating">Movie rating</span>
+      </div>
+      <div class="movie-data-row flexbox">
+        <span class="movie-duration">51 min</span>
+        <span class="movie-genres">Drama, Sci-fi</span>
+        <button class="add-btn" type="button">
+          Add to watchlist
+        </button>
+      </div>
+      <div class="movie-desc-row">
+        <p class="movie-desc">Movie description</p>
+      </div>
+    </div>
+  </div>
+</article>; */
